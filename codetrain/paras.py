ad63e078943bc9fcc7b89a1fd6cba00990c89695
Ziqian Wang <2779671262@qq.com>
2021-06-02 21:18:14 +0800

    fix some bugs

--- codetrain/paras.py
+++ codetrain/paras.py
@@ -19,11 +19,11 @@ PARAMETERS = {
 
 # 对战设置
 
-N = 5
+N = 10
 # 对战规模：N**len(parameters)
-K = 2
+K = 3
 # 每次对决进行2*k次
-PROCESS_LIMITS = 4
+PROCESS_LIMITS = None
 # 并行进程数。。。改一种方式，用多个进程跑同样的main代码，然后再进一步考察,None代表取为电脑核数
 # 若为1 则返回方式与原来相同
 WINNER_IS_KING = True
@@ -42,26 +42,27 @@ SCORES_FILE = 'scores.json'
 
 # 分析设置
 
-ANALYZE_SCORES = False
+ANALYZE_SCORES = True
 # 分析结果，并给出分布图（每个线程的）
 SHOW_EVERY_ANALYZE = False
 # 展示每一个分析的结果
-SAVE_EVERY_ANALYZE = False
+SAVE_EVERY_ANALYZE = True
+# 保存每一个分析结果
 
 
 # 预分析设置
 
-PRE_ANALYZE = False
+PRE_ANALYZE = True
 # 预分析：在给定区域内取15组值进行计算，给出分析结果(大约6分钟)
 PRE_ANALYZE_AUTO_SAVE = False
 # 预分析结果储存
-PRE_ANALYZE_AUTO_SHOW = False
+PRE_ANALYZE_AUTO_SHOW = True
 # 预分析结果展示
 
 
 # 服务器设置
 
-SERVER_MOD = True
+SERVER_MOD = False
 # 是否用服务器跑
 SEND_MAIL = False
 # 运算结束发送邮件
